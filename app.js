dd21b47f9708df8c635fcdcf9cf6796871252168
nasiri <futinasiri7@gmail.com>
2024-03-10 12:24:31 +0330

    fix number of product

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://shop-front-1.onrender.com/"
+  origin: "https://shop-front-1.onrender.com"
 }));
 // app.use(cors());
 app.use(helmet());
