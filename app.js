e8be182d2458f7c5cdfd05833c160f004ffad516
nasiri <futinasiri7@gmail.com>
2024-03-12 12:44:35 +0330

    fix cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 
 // app.use(express.json());
 app.use(cors({
-  origin: "https://clothingshop.onrender.com"
+  origin: "https://shop-front-black.vercel.app"
 }));
 app.use(cors());
 app.use(helmet());
